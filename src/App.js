6c0567d21d8c72eae584b96aaf9f0e0206b17070
marky ferreira <f.marky@escolar.ifrn.edu.br>
2026-07-10 14:21:40 -0300

    Adicionando os campos de busca dos jogadores para a mudança de pokemon.

--- src/App.js
+++ src/App.js
@@ -118,6 +118,15 @@ export default function Campo() {
         <h2>{jogadorDaVez}</h2>
       </div>
 
+      <div className="container-principal">
+
+        <div className="painel-jogador">
+    <h3>Jogador 1</h3>
+    <input type="text" placeholder="Nome do Pokemon" className="input-jogador" />
+    <button className="botao-busca">Buscar</button>
+       </div>
+
+       <div className="tabuleiro-centro">
       <div className="board-row">
         <Square valor={quadrados[0]} func={() => handleClick(0)} vencedor={linhaVencedora.includes(0)} />
         <Square valor={quadrados[1]} func={() => handleClick(1)} vencedor={linhaVencedora.includes(1)} />
@@ -134,6 +143,14 @@ export default function Campo() {
         <Square valor={quadrados[6]} func={() => handleClick(6)} vencedor={linhaVencedora.includes(6)} />
         <Square valor={quadrados[7]} func={() => handleClick(7)} vencedor={linhaVencedora.includes(7)} />
         <Square valor={quadrados[8]} func={() => handleClick(8)} vencedor={linhaVencedora.includes(8)} />
+      </div>
+      </div>
+
+      <div className="painel-jogador">
+      <h3>Jogador 2</h3>
+      <input type="text" placeholder="Nome do Jogador 2" className="input-jogador" />
+      <button className="botao-busca">Buscar</button>
+       </div>
       </div>
 
       <div>
